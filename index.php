7c2d7194a197d38a90b9b936de1447e6e0b8fde1
Fluha2 <152489586+Fluha2@users.noreply.github.com>
2023-11-30 11:33:11 +0100

    hallo ich bgins

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+
+  <style>
+    body{
+      margin:0
+    }
+  </style>
 </head>
 <body>
   <h1>
@@ -11,6 +17,3 @@
   </h1>
 </body>
 </html>
-body{
-  margin:0
-}
